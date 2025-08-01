f513b790614343664b33c328f89266c0634c5f61
Sabin24-max <sabinshrestha0624@gmail.com>
2025-08-01 11:50:46 +0545

    updated

---
+++ Backend/models/Contact.js
@@ -0,0 +1,25 @@
+export default (sequelize, DataTypes) => {
+  const Contact = sequelize.define('Contact', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+
+  return Contact;
+};
